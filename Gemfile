a6b38aaaac922eaef1943f68e3183c33d04725e1
Jacob <jacob@Macintosh-3.local>
2014-10-18 15:28:50 -0400

    Created the First Tweet

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
 end
+
+gem 'simple_form'
+#This is the the Gem we are using to create forms 
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,29 @@
 class TweetsController < ApplicationController
+	def new
+		@tweet = Tweet.new
+		@tweets = Tweet.all
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+
+		@tweets = Tweet.all
+		print "the tweet is" + @tweet.inspect.to_s
+
+		render "new"
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def index
+	end
+
+	def tweet_params
+		params.require("tweet").permit("content")
+	end
 end
